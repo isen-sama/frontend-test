8ba3e6d1f520ec5a5640b7f3552c20cd3068129c
tennousei.dandan <arnakorn567@gmail.com>
2024-07-31 16:26:05 +0700

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -9,18 +9,10 @@ export default function Navbar() {
         <a className="navbar-brand" href="#">
           <img src="/assets/img/top_logo_197x40.png" alt="..." width={197} height={40} className="d-inline-block align-text-top" />
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse justify-content-between">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link
